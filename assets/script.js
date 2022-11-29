a8a9e735808f09a2cc2f809af8915d402c9d0cb0
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2022-11-28 16:45:41 -0800

    add pseudo code

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// Welcome page with a "clickable" start button to start the game
+
+// Present with first question after clicking start button
+
+// Display and start timer after clicking start button
+
+// After question response is clicked, display "Right" or "Wrong" depending on response
+
+// If question is answered incorrectly, subtract 5 seconds from timer
+
+// End the game when all questions have been answered or the timer runs out
+
+// Prompt user to save initials and score
+
+// Browser should recall initials and score even when game is closed out of
